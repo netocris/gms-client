c7dd4ed35be28b0fec9f79195aab03b0d6ac8300
Cristóvão Neto <netocris@gmail.com>
2019-07-10 22:53:21 +0100

    storybook button

--- src/app/components/atoms/button/button.stories.ts
+++ src/app/components/atoms/button/button.stories.ts
@@ -1,8 +1,9 @@
 import { storiesOf, moduleMetadata } from '@storybook/angular';
-import { action } from '@storybook/addon-actions';
+import { action} from '@storybook/addon-actions';
+import { text, boolean, withKnobs } from '@storybook/addon-knobs';
 import { ButtonComponent } from './button.component';
 
-storiesOf('Button', module)
+storiesOf('Components|Button', module)
   .addDecorator(
     moduleMetadata({
       declarations: [ButtonComponent]
@@ -15,25 +16,27 @@ storiesOf('Button', module)
     }
   }))
   .add('disabled', () => ({
-      template: `<app-button [valueModel]="valueModel" [disabled]="disabled"></app-button>`,
-      props: {
-        valueModel: 'Click',
-        disabled: true
-      }
-    }),
-    { 
-      notes: 'disabled button' 
+    template: `<app-button [valueModel]="valueModel" [disabled]="disabled"></app-button>`,
+    props: {
+      valueModel: 'Click',
+      disabled: true
     }
-  )
+  }))
   .add('with action', () => ({
-      template: `<app-button [valueModel]="valueModel" [disabled]="disabled" (buttonEventEmmitter)="buttonEventEmmitter($event)"></app-button>`,
-      props: {
-        valueModel: 'Click',
-        disabled: false,
-        buttonEventEmmitter: action('buttonEventEmmitter')
-      }
-    }),
-    { 
-      notes: 'some action with button onclick event' 
+    template: `<app-button [valueModel]="valueModel" [disabled]="disabled" (buttonEventEmmitter)="buttonEventEmmitter($event)"></app-button>`,
+    props: {
+      valueModel: 'Click',
+      disabled: false,
+      buttonEventEmmitter: action('buttonEventEmmitter')
     }
-  );
+  }));
+
+storiesOf('Components|Button', module)
+  .addDecorator(withKnobs)
+  .add('customization', () => ({
+    component: ButtonComponent,
+    props: {
+      valueModel: text('valueModel', 'Click'),
+      disabled: boolean('disabled', false)      
+    }
+  }));
